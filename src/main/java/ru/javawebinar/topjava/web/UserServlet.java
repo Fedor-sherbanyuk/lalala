aacd3faa737b42e6d7f6aab179479f1b7e9c3060
fedor-sherbanyuk <fedor.sherbanyuk1@gmail.com>
2022-06-05 12:51:24 +0300

    add servlet and jsp/html

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -1,14 +1,25 @@
 package ru.javawebinar.topjava.web;
 
+
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import java.io.IOException;
 
 public class UserServlet extends HttpServlet {
+
+    private static final Logger LOG = LoggerFactory.getLogger(UserServlet.class);
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        request.getRequestDispatcher("/users.jsp").forward(request,response);
+        LOG.debug("redirect to userList");
+        response.sendRedirect("users.jsp");
+
     }
 
 //    @Override
